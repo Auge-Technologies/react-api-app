f93fddc4603c49a331a4ddfd3e78aa58976c6975
charlohc <Charlottee.Christiansen@gmail.com>
2023-07-06 18:14:33 +0200

    implemented simple loggin and routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
-import './App.css';
-import Skill_input from './components/Skill_input';
-import Skill_search from './components/Skill_search';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
+import About from './views/About';
+import Home from './views/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <Skill_input/>
-      <Skill_search/>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                {/*<ul>
+                     ...your existing navigation links...
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                </ul>*/}
+
+                {/* Define the routes */}
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
-export default App;
+function Auth0App() {
+    const domain = 'auge.eu.auth0.com';
+    const clientId = 'nwfr9B2bb0IePJjWSKhzp6YgOxTZhwvN';
+
+    return (
+        <Auth0Provider
+            domain={domain}
+            clientId={clientId}
+            redirectUri={window.location.origin}
+        >
+            <App />
+        </Auth0Provider>
+    );
+}
+
+export default Auth0App;
