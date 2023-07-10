32e430d8557226c5e9dc70a794e674c682bf3d1b
charlohc <Charlottee.Christiansen@gmail.com>
2023-07-10 11:58:17 +0200

    changed the routing to log in

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ function App() {
 
                 {/* Define the routes */}
                 <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/about" element={<About />} />
+                    {<Route path="/login" element={<Home />} />}
+                    <Route path="/" element={<About />} />
                 </Routes>
             </div>
         </Router>
