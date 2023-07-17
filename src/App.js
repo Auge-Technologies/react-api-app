0a5478f587dc7a19b4fde63f18eb8633169f4833
haraldpaaske <haraldpaaske@gmail.com>
2023-07-17 11:16:51 +0200

    profile update

--- src/App.js
+++ src/App.js
@@ -1,46 +1,46 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { Auth0Provider } from '@auth0/auth0-react';
-import Admin from './views/Admin';
-import Home from './views/Home';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
+import Admin from "./views/Admin";
+import Home from "./views/Home";
 import Dashboard from "./views/Dashboard";
 import Profile from "./views/Profile";
 
 function App() {
-    return (
-        <Router>
-            <div className="App">
-                {/*<ul>
+  return (
+    <Router>
+      <div className="App">
+        {/*<ul>
                      ...your existing navigation links...
                     <li>
                         <Link to="/about">Admin</Link>
                     </li>
                 </ul>*/}
 
-                {/* Define the routes */}
-                <Routes>
-                    {<Route path="/login" element={<Home />} />}
-                    <Route path="/" element={<Admin />} />
-                    <Route path="/dashboard" element={<Dashboard />} />
-                    <Route path="/profile" element={<Profile />} />
-                </Routes>
-            </div>
-        </Router>
-    );
+        {/* Define the routes */}
+        <Routes>
+          {<Route path="/login" element={<Home />} />}
+          <Route path="/Admin" element={<Admin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/" element={<Profile />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 function Auth0App() {
-    const domain = 'auge.eu.auth0.com';
-    const clientId = 'nwfr9B2bb0IePJjWSKhzp6YgOxTZhwvN';
+  const domain = "auge.eu.auth0.com";
+  const clientId = "nwfr9B2bb0IePJjWSKhzp6YgOxTZhwvN";
 
-    return (
-        <Auth0Provider
-            domain={domain}
-            clientId={clientId}
-            redirectUri={window.location.origin}
-        >
-            <App />
-        </Auth0Provider>
-    );
+  return (
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
+  );
 }
 
 export default Auth0App;
--- src/views/Profile.js
+++ src/views/Profile.js
@@ -1,74 +1,153 @@
-import React, { useState, useEffect } from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import qs from "qs";
 
-const Profile = () => {
-    const { logout } = useAuth0();
-    const [skills, setSkills] = useState([]);
-    const [roles, setRoles] = useState([]);
+const Profile = (props) => {
+  const [relatedSkills, setRelatedSkills] = useState([]);
+  const [knownSkills, setKnownSkills] = useState([]);
+  const [expandedSkills, setExpandedSkills] = useState([]);
+  const [isExpanded, setIsExpanded] = useState([]);
+  const [employee, setEmployee] = useState([]);
 
-    const handleLogoutClick = () => {
-        logout();
+  const getAccessToken = async () => {
+    const authData = {
+      client_id: "stq2qi97wjfeyjye",
+      client_secret: "iKDBB7ZI",
+      grant_type: "client_credentials",
+      scope: "emsi_open",
     };
 
-    const fetchEmployeeSkills = async (employeeId) => {
-        try {
-            const response = await axios.get(
-                `http://localhost:8080/employee/skills/${employeeId}`
-            );
-            const skillsData = response.data;
-            setSkills(skillsData);
-        } catch (error) {
-            console.error(error);
+    try {
+      const response = await axios.post(
+        "https://auth.emsicloud.com/connect/token",
+        qs.stringify(authData)
+      );
+      const accessToken = response.data.access_token;
+      return accessToken;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  };
+
+  const FindEmployees = async () => {
+    try {
+      console.log(props.name);
+      const response = await axios.get(`http://localhost:8080/employees/1`);
+      const employee = response.data.filter(
+        (employee) => employee.name === props.name
+      );
+      setEmployee(employee);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const findKnownSkills = async () => {
+    try {
+      const id = employee.map((a) => a.id);
+      console.log(employee);
+      console.log(id);
+      const response = await axios.get(
+        `http://localhost:8080/employee/skills/${id}`
+      ); // Replace with your actual Spring Boot endpoint
+
+      const skills = response.data;
+
+      const uniqueSkills = skills.reduce((acc, skill) => {
+        if (!acc.find((item) => item.name === skill.name)) {
+          acc.push(skill);
         }
-    };
+        return acc;
+      }, []);
 
-    const fetchEmployeeRoles = async (employeeId) => {
-        try {
-            const response = await axios.get(
-                `http://localhost:8080/employee/roles/${employeeId}`
-            );
-            const rolesData = response.data;
-            setRoles(rolesData);
-        } catch (error) {
-            console.error(error);
-        }
-    };
+      setKnownSkills(uniqueSkills);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    useEffect(() => {
-        // TODO: use info retained from login
-        fetchEmployeeSkills(4);
-        fetchEmployeeRoles(4)
-    }, []);
+  const getRelatedSkills = async (skills, setSkills) => {
+    console.log(skills);
+    try {
+      const accessToken = await getAccessToken();
 
-    return (
-        <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
-            <div style={{ flex: '1 1 auto' }}>
-                <h1>Profile Page</h1>
-                <div style={{ display: 'flex' }}>
-                    <div>
-                        <p>Known skills:</p>
-                        <ul>
-                            {skills.map((skill) => (
-                                <li key={skill.id}>{skill.name}</li>
-                            ))}
-                        </ul>
-                    </div>
-                    <div>
-                        <p>Roles:</p>
-                        <ul>
-                            {roles.map((role) => (
-                                <li key={role.id}>{role.name}</li>
-                            ))}
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div>
-                <button onClick={handleLogoutClick}>Logout</button>
-            </div>
-        </div>
-    );
+      if (accessToken && skills.length > 0) {
+        const response = await axios.post(
+          "https://emsiservices.com/skills/versions/latest/related",
+          {
+            ids: skills.map((skill) => skill.id),
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        const relatedSkillsData = response.data.data || [];
+        setSkills(relatedSkillsData);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    findKnownSkills();
+  }, [employee]);
+
+  useEffect(() => {
+    FindEmployees();
+  }, []);
+
+  const handleExpand = (index) => {
+    if (isExpanded.includes(index)) {
+      setIsExpanded((prevState) => prevState.filter((item) => item !== index));
+    } else {
+      getRelatedSkills([relatedSkills[index]], setExpandedSkills);
+      setIsExpanded((prevState) => [...prevState, index]);
+    }
+  };
+
+  return (
+    <>
+      <h2>You know:</h2>
+      <ul>
+        {knownSkills.map((skill, index) => (
+          <li key={index}>{skill.name}</li>
+        ))}
+      </ul>
+
+      <button onClick={() => getRelatedSkills(knownSkills, setRelatedSkills)}>
+        Give me similar skills to what I know
+      </button>
+
+      <h2>You might want to learn:</h2>
+      <ul>
+        {relatedSkills.map((skill, index) => (
+          <li key={index}>
+            {skill.name}{" "}
+            <button
+              onClick={() => {
+                handleExpand(index);
+              }}
+            >
+              expand
+            </button>
+            {isExpanded.includes(index) && (
+              <ul>
+                {expandedSkills.map((skill, index) => (
+                  <li key={index}>{skill.name}</li>
+                ))}
+              </ul>
+            )}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default Profile;
